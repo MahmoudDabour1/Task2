c5ba6240a72a1685dabedc66245e84a7839d85bb
MahmoudDabour1 <mahmoud.dabour1000000@gmail.com>
2023-02-04 00:24:18 +0200

    first commit

---
+++ lib/views/view.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ImageTask extends StatelessWidget {
+  const ImageTask({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MyApp"),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset("assets/Images/download.jpeg"),
+              SizedBox(
+                height: 10,
+              ),
+              Image.asset("assets/Images/download (1).jpeg"),
+              SizedBox(
+                height: 10,
+              ),
+              Image.network("https://avatars.mds.yandex.net/i?id=84dbd50839c3d640ebfc0de20994c30d-4473719-images-taas-consumers&n=27&h=480&w=480"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
